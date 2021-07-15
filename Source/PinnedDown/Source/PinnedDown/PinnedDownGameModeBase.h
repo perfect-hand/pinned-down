f940c79db578d927d63b4c245323967db90b119b
Nick Pruehs <dev@npruehs.de>
2021-07-15 23:39:42 +0200

    Add initial project files.

---
+++ Source/PinnedDown/Source/PinnedDown/PinnedDownGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PinnedDownGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PINNEDDOWN_API APinnedDownGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
