14e7e349aafb0db44316daefe9e21989b44356c7
Nick Pruehs <dev@npruehs.de>
2021-07-29 20:20:25 +0200

    Add initial plugin files.

---
+++ Source/PinnedDown/Plugins/CardGame/Source/CardGame/Classes/CardGameAttribute.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CardGameAttribute.generated.h"
+
+UCLASS()
+class CARDGAME_API UCardGameAttribute : public UDataAsset
+{
+	GENERATED_BODY()
+};
---
+++ Source/PinnedDown/Plugins/CardGame/Source/CardGame/Classes/CardGameCardPile.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "CardGameScope.h"
+#include "CardGameCardPile.generated.h"
+
+UCLASS()
+class CARDGAME_API UCardGameCardPile : public UDataAsset
+{
+	GENERATED_BODY()
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	ECardGameScope Scope;
+};
